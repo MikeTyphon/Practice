8f74d6a76cc4d7275a4e87356cee1b6c2637ae89
MikeTyphon <40417986+MikeTyphon@users.noreply.github.com>
2018-07-31 00:39:02 -0700

    Nerp

--- git.py
+++ git.py
@@ -8,3 +8,10 @@ x = 'there once was a chicken from penisville'
 
 bananna = 'this is from the bananna branch'
 b = 'for bannana'
+nerp = 'nerp'
+narp = 'doubler nothing nerp nerp'
+nerp = nerp *100000
+print(nerp)
+
+print(nerp ==nerp)
+print(narp == nerp)
